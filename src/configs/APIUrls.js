3870fac5817abe2f2a57b98a261264443d94090b
Vitalii Melnik <mvitalim@gmail.com>
2023-09-14 22:13:17 +0300

    add "react-scripts": "5.0.1"

--- src/configs/APIUrls.js
+++ src/configs/APIUrls.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.REACT_APP_baseURL;
+const API_URL = (process.env.NODE_ENV === "production") ? process.env.REACT_APP_baseURL : process.env.REACT_APP_localURL;
 
 export const APIUrls = {
     words: `${API_URL}/words`,
